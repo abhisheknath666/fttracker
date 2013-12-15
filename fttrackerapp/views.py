67d26d9194a4b34dc97631739bf284b54514be96
Abhishek Nath <illidan.elf@gmail.com>
2013-12-15 11:51:16 -0800

    Prettify the html

--- fttrackerapp/views.py
+++ fttrackerapp/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from fttrackerapp.data_fetchers import FoodTruckDataFetcher
 
 def index(request):
-    message = "App that tracks food trucks around San Francisco's financial district"
+    message = """<h3>Welcome to Food Truck Tracker!</h3><p>We aggregate food truck visits around San Francisco's financial district.<br /><br /><a href="/pollfortrucks/">Click here</a> for a list of trucks participating in Off the grid over the last 30 days.</p>"""
     return HttpResponse(message)
 
 def poll_for_trucks(request):
